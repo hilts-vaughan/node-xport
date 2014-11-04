c10eef6a2124adaa8f82c1c362862a831b151cfe
PandaCoder <rjlodico@ncsu.edu>
2014-11-03 23:13:42 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+node-xport
+==========
+
+[![Build Status][image-npm]][url-npm]
+[![Build Status][image-tra]][url-tra]
+[![Build Status][image-lic]][url-lic]
+[![Build Status][image-dls]][url-dls]
+
+Using node-xport in a project
+-----------------------------
+```js
+var xport = require('node-xport');
+
+var myExports = {};
+
+xport(module, myExports);
+
+```
+
+[image-npm]: https://img.shields.io/npm/v/node-xport.svg?style=flat
+[image-tra]: http://img.shields.io/travis/pandacoder/node-xport.svg?style=flat
+[image-lic]: http://img.shields.io/npm/l/express.svg?style=flat
+[image-dls]: https://img.shields.io/npm/dm/node-xport.svg?style=flat
+
+[url-npm]: https://npmjs.org/package/node-xport
+[url-tra]: https://travis-ci.org/pandacoder/node-xport
+[url-lic]: https://github.com/PandaCoder/node-xport/blob/master/LICENSE
+[url-dls]: https://npmjs.org/package/node-xport
---
+++ xport.js
@@ -0,0 +1,26 @@
+/*
+    node-xport, a module export (simplifier?) utility package for node.js
+    Copyright (C) 2014  Robert Lodico (lodico.rj@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var xport = function(module, exports) {
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = exports;
+    }
+};
+
+/* Export the module */
+xport(module, xport);
